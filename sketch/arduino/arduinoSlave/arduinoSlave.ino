15d4390727a9f6f394e9ee48c7c04f985d86d6c6
CodeMaster <miguel230714@gmail.comnono>
2020-01-25 16:16:24 -0500

    Update arduinoSlave.ino
    Agregado Codigo de Sensor de Humedad

--- sketch/arduino/arduinoSlave/arduinoSlave.ino
+++ sketch/arduino/arduinoSlave/arduinoSlave.ino
@@ -1,19 +1,84 @@
+// Comumication to NODEMCU
 #include <SoftwareSerial.h>
 
 SoftwareSerial ArduinoSerial(2, 3); //(Rx Tx) ESP -> Arduino: Tx -> 2, Rx -> 3.
 char data;
 
+// Configuration of humidity (COH)
+#include "DHT.h"
+
+#define DHTPIN 5
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+
 void setup() {
+
   Serial.begin(115200);
   ArduinoSerial.begin(9600); //bd from master (ESP)
   pinMode(4, OUTPUT);
+
+
+  //COH
+  dht.begin();
+
 }
 
 void loop()
 {
   checkSerialCom();
-  delay(50);
+  readHumiditySensor();
+  readTemperatureSensor();
+
 }
+
+
+void readHumiditySensor() {
+
+  float h = dht.readHumidity();
+
+  if (isnan(h)) {
+    Serial.println(F("Failed to read Humidity from DHT sensor!"));
+    return;
+  }
+
+
+
+  Serial.print(F("Humidity: "));
+  Serial.println(h);
+
+
+  ArduinoSerial.print(F("Humidity: "));
+  ArduinoSerial.print(h);
+  ArduinoSerial.println("%");
+
+  delay(500);
+
+}
+
+
+void readTemperatureSensor() {
+
+  float t = dht.readTemperature();
+
+  if (isnan(t)) {
+    Serial.println(F("Failed to read Temperature from DHT sensor!"));
+    return;
+
+  }
+
+  Serial.print(F("Temperature: "));
+  Serial.print(t);
+  Serial.println(F("°C "));
+
+  ArduinoSerial.print(F("Temperature: "));
+  ArduinoSerial.print(t);
+  ArduinoSerial.println(F("°C "));
+
+  delay(500);
+
+}
+
 
 void checkSerialCom() {
   String response;
@@ -28,12 +93,14 @@ void checkSerialCom() {
 
     if (response == "on") {
       digitalWrite(4, LOW);
-      ArduinoSerial.print("Turned on");
+      ArduinoSerial.println("Turned on");
     }
 
     if (response == "off") {
       digitalWrite(4, HIGH);
-      ArduinoSerial.print("Turned off");
+      ArduinoSerial.println("Turned off");
     }
   }
+
+  delay(500);
 }
